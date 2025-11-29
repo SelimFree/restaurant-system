4c575c1a46eb7e70c9ada11b0f66a3865d42a06e
Selim Altayev <selim.altayev@gmail.com>
2025-11-29 22:15:29 +0100

    added role based autharization

--- app/api/deps.py
+++ app/api/deps.py
@@ -3,6 +3,7 @@ from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from jose import jwt, JWTError
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
+from typing import List, Optional
 
 from core.config import settings
 from db.database import get_db
@@ -41,3 +42,18 @@ async def get_current_user(
         raise credentials_exception
     
     return user
+
+def role_required(required_roles: Optional[List[str]] = None):
+    if required_roles is None:
+        required_roles = []
+        
+    def role_checker(current_user: User = Depends(get_current_user)):
+        if required_roles and current_user.role not in required_roles:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="You do not have permission to access this resource.",
+            )
+        
+        return current_user
+    
+    return role_checker
--- app/api/v1/endpoints/restaurants.py
+++ app/api/v1/endpoints/restaurants.py
@@ -7,13 +7,13 @@ from db.database import get_db
 from models.restaurants import Restaurant
 from models.users import User
 from schemas.restaurants import RestaurantRead
-from api.deps import get_current_user
+from api.deps import role_required
 
 router = APIRouter()
 
 @router.get("/restaurants", response_model=List[RestaurantRead])
 async def get_all_restaurants(
-    current_user: User = Depends(get_current_user),
+    current_user: User = Depends(role_required(["admin", "manager"])),
     db: AsyncSession = Depends(get_db)
 ):
     stmt = select(Restaurant)
