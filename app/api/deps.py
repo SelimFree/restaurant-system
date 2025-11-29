bf0ba0028267a63728f41b43bff58e72d4a7634d
Selim Altayev <selim.altayev@gmail.com>
2025-11-29 21:48:55 +0100

    Added example protected routes

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,17 +1,17 @@
 from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials 
 from jose import jwt, JWTError
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
-from core import security
+from core.config import settings
 from db.database import get_db
-from models.user import User
+from models.users import User
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login")
+bearer_scheme = HTTPBearer(scheme_name="Bearer Token Login")
 
 async def get_current_user(
-    token: str = Depends(oauth2_scheme),
+    token_credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme),
     db: AsyncSession = Depends(get_db)
 ) -> User:
     credentials_exception = HTTPException(
@@ -19,11 +19,14 @@ async def get_current_user(
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
+    
+    token = token_credentials.credentials 
+
     try:
         payload = jwt.decode(
             token, 
-            security.SECRET_KEY, 
-            algorithms=[security.ALGORITHM]
+            settings.SECRET_KEY, 
+            algorithms=[settings.ALGORITHM]
         )
         email: str = payload.get("sub")
         if email is None:
